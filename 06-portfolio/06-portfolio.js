f58e1926eb0e6e83cd9cfbf1f29698145a4a50b2
Niko192001 <nikolaj@rahe.dk>
2025-12-05 13:44:02 +0100

    added a function and a screenshot (can be deletead later)

---
+++ 06-portfolio/06-portfolio.js
@@ -0,0 +1,4 @@
+document.getElementById ("lon").addEventListener("click",function(){
+    const img = document.getElementById("LonImage");
+    img.style.display = (img.style.display === "none") ? "block": "none";
+});
