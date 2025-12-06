657c54d2fd4f259d3a3700294357a857bb6c7978
Niko192001 <nikolaj@rahe.dk>
2025-12-06 13:03:20 +0100

    Added a new function with a chart (can be removed or edit with)

--- 06-portfolio/06-portfolio.js
+++ 06-portfolio/06-portfolio.js
@@ -1,4 +1,30 @@
-document.getElementById ("lon").addEventListener("click",function(){
-    const img = document.getElementById("LonImage");
-    img.style.display = (img.style.display === "none") ? "block": "none";
+document.getElementById("lon").addEventListener("click", function(){
+    const canvas =document.getElementById("genderChart");
+    canvas.style.display = (canvas.style.display === "none") ? "block" : "none";
+
+    new Chart(canvas, {
+        type: 'bar',
+        data: {
+            labels: ['Sundhed', 'Undervisning', 'Pleje','Administration','IT-Branchen'],
+            datasets: [{
+                label: 'Antal Kvinder (%)',
+                data:[80,70,75,65,28], //offcielle tendeser fra kilder
+                backgroundColor:[
+                    '#ff9999',
+                    '#ffcc99',
+                    '#99ccff',
+                    '#cc99ff',
+                    '#66cc99'
+                ]
+            }]
+        },
+        options:{
+            scales:{
+                y:{
+                    beginAtZero: true,
+                    max: 100
+                }
+            }
+        }
+});
 });
